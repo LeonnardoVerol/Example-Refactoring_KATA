2ce46bc9c53caa929d3ad99f1a8b5352a569b752
Leonnardo Verol <6124410+LeonnardoVerol@users.noreply.github.com>
2018-09-28 00:43:00 -0300

    Create TennisGame Class

---
+++ Tennis/Source/TennisGame.cs
@@ -0,0 +1,17 @@
+﻿namespace Tennis
+{
+	public class TennisGame
+	{
+		private int player1point;
+		private int player2point;
+		
+		private string player1Name;
+		private string player2Name;
+
+		public TennisGame(string player1Name, string player2Name)
+		{
+			this.player1Name = player1Name;
+			this.player2Name = player2Name;
+		}
+	}
+}
