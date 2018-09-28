208bc7cca303f1cc4f8b455157581608abd7f073
Leonnardo Verol <6124410+LeonnardoVerol@users.noreply.github.com>
2018-09-28 01:46:33 -0300

    Improve Code

--- Tennis/Source/TennisGame.cs
+++ Tennis/Source/TennisGame.cs
@@ -37,9 +37,34 @@
 				return "Deuce";
 			else
 			{
-				string s = player1point > player2point ? player1Name : player2Name;
-				return ((player1point - player2point) * (player1point - player2point) == 1) ? "Advantage " + s : "Win for " + s;
+				string score = "";
+
+				if (player1point > player2point && player2point >= 3)
+				{
+					score = "Advantage player1";
+				}
+
+				if (player2point > player1point && player1point >= 3)
+				{
+					score = "Advantage player2";
+				}
+
+				if (player1point >= 4 && player2point >= 0 && (player1point - player2point) >= 2)
+				{
+					score = "Win for player1";
+				}
+				if (player2point >= 4 && player1point >= 0 && (player2point - player1point) >= 2)
+				{
+					score = "Win for player2";
+				}
+
+				return score;
 			}
+		}
+
+		public int GetTotalPoints()
+		{
+			return player1point + player2point;
 		}
 	}
 }
