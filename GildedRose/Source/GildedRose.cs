f8c226fb6a3a1fbecfdf2ccc25947f2513f35717
Leonnardo Verol <6124410+LeonnardoVerol@users.noreply.github.com>
2018-09-27 21:49:50 -0300

    Consolidate Duplicate Conditional Fragments
    Simplifying Conditional Expressions

--- GildedRose/Source/GildedRose.cs
+++ GildedRose/Source/GildedRose.cs
@@ -33,23 +33,11 @@ namespace csharp
 		{
 			if (Items[i].SellIn < 0)
 			{
-				if (Items[i].Name != "Aged Brie")
+				DecreaseCommonItemQuality(i);
+				
+				if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
 				{
-					if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-					{
-						if (Items[i].Quality > 0) //-1 geral (o dobro qnd expirou)
-						{
-							if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-							{
-								Items[i].Quality = Items[i].Quality - 1;
-							}
-						}
-					}
-
-					if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-					{
-						Items[i].Quality = Items[i].Quality - Items[i].Quality;
-					}
+					Items[i].Quality = Items[i].Quality - Items[i].Quality;
 				}
 
 				// +1 aged brie ( o dobro)
